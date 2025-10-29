62a0b95e14a39aa558035884df65083607299d5a
Edko21 <146870569+Edko21@users.noreply.github.com>
2025-10-29 14:00:02 +0200

    Add files via upload

---
+++ Queue/LinkedQueue.h
@@ -0,0 +1,120 @@
+#include <iostream>
+
+template<typename T>
+class LinkedQueue {
+public:
+	LinkedQueue();
+	LinkedQueue(const LinkedQueue& other);
+	LinkedQueue& operator=(const LinkedQueue& other);
+	~LinkedQueue();
+
+	void enqueue(const T& el);
+	void dequeue();
+	const T& peek() const;
+	bool isEmpty() const;
+
+private:
+	struct Node {
+		Node(T data) {
+			this->data = data;
+			next = nullptr;
+		}
+		T data;
+		Node* next;
+	};
+
+	Node* tail;
+	Node* head;
+
+	void free();
+	void copyFrom(const LinkedQueue& other);
+};	
+
+template<typename T>
+void LinkedQueue<T>::free() {
+	Node* iter = head;
+	while (iter != nullptr) {
+		Node* prev = iter;
+		iter = iter->next;
+		delete prev;
+	}
+	head = tail = nullptr;
+}
+
+template<typename T>
+void LinkedQueue<T>::copyFrom(const LinkedQueue& other) {
+	Node* iter = other.head;
+	while (iter != nullptr) {
+		enqueue(iter->data);
+		iter = iter->next;
+	}
+}
+
+template<typename T>
+LinkedQueue<T>::LinkedQueue() {
+	head = nullptr;
+	tail = nullptr;
+}
+
+template<typename T>
+LinkedQueue<T>::LinkedQueue(const LinkedQueue& other) {
+	copyFrom(other);
+}
+
+template<typename T>
+LinkedQueue<T>::~LinkedQueue() {
+	free();
+}
+
+template<typename T>
+LinkedQueue<T>& LinkedQueue<T>::operator=(const LinkedQueue& other) {
+	if (this != &other) {
+		free();
+		copyFrom(other);
+	}
+	return *this;
+}
+
+template<typename T>
+void LinkedQueue<T>::enqueue(const T& el) {
+	Node* newNode = new Node(el);
+	if (isEmpty()) {
+		head = newNode;
+		tail = newNode;
+	}
+	else {
+		tail->next = newNode;
+		tail = newNode;
+	}
+}
+
+template<typename T>
+void LinkedQueue<T>::dequeue() {
+	if (isEmpty())
+		throw std::runtime_error("list empty");
+	else if (head == tail) {
+		T el = head->data;
+		delete head;
+
+		head = nullptr;
+		tail = nullptr;
+	}
+	else{
+		Node* temp = head->next;
+		delete head;
+		head = temp;
+	}	
+}
+
+template<typename T>
+const T& LinkedQueue<T>::peek() const {
+	if (isEmpty())
+		throw std::runtime_error("list emty");
+	return head->data;
+}
+
+template<typename T>
+bool LinkedQueue<T>::isEmpty() const {
+	return head = nullptr && tail = nullptr;
+}
+
